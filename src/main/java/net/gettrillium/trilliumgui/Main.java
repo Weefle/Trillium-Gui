32d479243fe6ec25c96422bc5499781d065ba9fc
Saad Ardati <saadodi44@gmail.com>
2015-06-28 20:36:57 +0300

    Added stuff

--- src/main/java/net/gettrillium/trilliumgui/Main.java
+++ src/main/java/net/gettrillium/trilliumgui/Main.java
@@ -1,4 +1,19 @@
 package net.gettrillium.trilliumgui;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
+    private static JFrame frame = new JFrame("Trillium Configurator");
+
+    public static void main(String[] args) {
+        frame.setResizable(true);
+        frame.setSize(new Dimension(500, 720));
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
 }
